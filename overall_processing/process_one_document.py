c6bc8006db06f6e35c147b35661fdde97d4f2cc3
honza <jan.kubant@blindspot.ai>
2020-05-03 15:21:27 +0200

    added data outputting

--- overall_processing/process_one_document.py
+++ overall_processing/process_one_document.py
@@ -1,6 +1,9 @@
 import json
 
 import cv2
+import numpy as np
+import pandas as pd
+import os
 
 from field_recognizer.recognize_all import recognize
 from preprocessing.preprocess import preprocess
@@ -17,6 +20,27 @@ def process_document(im, config):
     return form_data
 
 
+def output_data(form_data, name):
+    d = dict()
+    fil = form_data["fields"]
+    for field in fil:
+        if field["type"] == "boxes":
+            data = np.argmax(field["recognized"])
+        elif field["type"] == "letters":
+            data = "".join(field["recognized"])
+
+        field_name = field["name"] + field["type"]
+        d[f'{field_name}_data'] = data,
+        d[f'{field_name}_acc'] = min(field["accuracy"])
+        d[f'{field_name}_img'] = f"{field_name}_img.png"
+        cv2.imwrite(os.path.join(os.path.dirname(name), f"{field_name}_img.png"), field["img"])
+
+    df = pd.DataFrame(d)
+    filename = f"{name.split('.')[0]}.xlsx"
+    df.to_excel(filename, index=False)
+    return filename
+
+
 if __name__ == '__main__':
     image_path = "test/example_forms/julinka_dotaznik/front_page.jpg"
     with open("test/example_forms/julinka_dotaznik/front_page_config.json", 'r') as f:
@@ -24,3 +48,4 @@ if __name__ == '__main__':
     im = cv2.imread(image_path)
 
     form_data = process_document(im, config)
+    filename = output_data(form_data, image_path)
