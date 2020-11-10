3aa92118168c1dfa107f3af64b3a0b5d6c5f8b93
Jan Kubant <jan.kubant@blindspot.ai>
2020-11-10 11:43:23 +0100

    Some renamings

--- outputting/process_document.py
+++ outputting/process_document.py
@@ -11,6 +11,13 @@ from preprocessing.templating import get_templates
 from structure_parser.form_structure_parser import FormStructureParser, FormData
 from utils.image_loading import load_images_from_path
 
+LETTERS_MODEL_JSON = "model_letters.json"
+LETTERS_MODEL_H5 = "model_letters.h5"
+NUMBERS_MODEL_JSON = "model_numbers.json"
+NUMBERS_MODEL_H5 = "model_numbers.h5"
+LETTER_MAPPER_JSON = "model_letters_mapping.json"
+NUMBER_MAPPER_JSON = "model_numbers_mapping.json"
+
 
 def load_path_configuration(path_configuration) -> PathConfig:
     with open(path_configuration, "rb") as f:
@@ -18,12 +25,12 @@ def load_path_configuration(path_configuration) -> PathConfig:
 
 
 def load_models(model_data_location: str):
-    model_letters_structure_path = f"{model_data_location}/model_letters.json"
-    model_letters_weights_path = f"{model_data_location}/model_letters.h5"
-    model_numbers_structure_path = f"{model_data_location}/model_numbers.json"
-    model_numbers_weights_path = f"{model_data_location}/model_numbers.h5"
-    letter_mapper_path = f"{model_data_location}/model_letters_mapping.json"
-    number_mapper_path = f"{model_data_location}/model_numbers_mapping.json"
+    model_letters_structure_path = f"{model_data_location}/{LETTERS_MODEL_JSON}"
+    model_letters_weights_path = f"{model_data_location}/{LETTERS_MODEL_H5}"
+    model_numbers_structure_path = f"{model_data_location}/{NUMBERS_MODEL_JSON}"
+    model_numbers_weights_path = f"{model_data_location}/{NUMBERS_MODEL_H5}"
+    letter_mapper_path = f"{model_data_location}/{LETTER_MAPPER_JSON}"
+    number_mapper_path = f"{model_data_location}/{NUMBER_MAPPER_JSON}"
 
     model_letters = load_model(model_letters_structure_path, model_letters_weights_path)
     model_numbers = load_model(model_numbers_structure_path, model_numbers_weights_path)
